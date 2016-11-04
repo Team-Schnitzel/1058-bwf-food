84309f22fe1af72e16e563c376c0cdcd9e315c5e
Mark Pashmfouroush <MarkPash@users.noreply.github.com>
2016-11-04 22:25:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -38,14 +38,14 @@ def logToFile(person,foodItemId):
     
 def sendRequest(person,foodItemId):
     requestsSession = requests.session()
-    postData1 = {"username": person[0], "password": person[1], "login": "Aanmelden"}
-    requestsSession.post(PostUrl, data=postData1, verify=False)
+    postData = {"username": person[0], "password": person[1], "login": "Aanmelden"}
+    requestsSession.post(PostUrl, header=headers, data=postData, verify=False)
     time.sleep(1)
-    postData2 = {"order_item": str(16), "order_item_add": "Voeg Toe"}
-    requestsSession.post(PostUrl, data=postData2, verify=False)
+    postData = {"order_item": str(16), "order_item_add": "Voeg Toe"}
+    requestsSession.post(PostUrl, header=headers, data=postData, verify=False)
     time.sleep(1)
-    PostData3 = {"opmerkingen": " ", "user_order_start_print": "Plaats Bestelling"}
-    requestsSession.post(PostUrl, data=postData3, verify=False)
+    postData = {"opmerkingen": " ", "user_order_start_print": "Plaats Bestelling"}
+    requestsSession.post(PostUrl, header=headers, data=postData, verify=False)
     
     logToFile(person,foodItemId)
     print("Order placed  1x {0:10} for {1:10} at {2}".format(foodItems[foodItemIds.index(foodItemId)],person[0],str(datetime.now())))
